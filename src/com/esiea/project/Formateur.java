152150d8dd3d3ffae8715f7a4686fa2c72946a7e
bienvenu <bienvenuquentin@gmail.com>
2015-02-26 23:58:36 +0100

    Date Done

--- src/com/esiea/project/Formateur.java
+++ src/com/esiea/project/Formateur.java
@@ -1,11 +1,38 @@
 package com.esiea.project;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.text.DateFormat;
+import java.util.Locale;
+
 public class Formateur extends LoggerFactory{
 	
 	
 	public void getFormate(Logger logger, Level level){
 		
-		System.out.print("\n[NAME="+logger.getName());
+		
+		Date aujourdhui = new Date();
+		
+	
+		
+		DateFormat mediumDateFormat = DateFormat.getDateTimeInstance(
+		DateFormat.MEDIUM,
+		
+		DateFormat.MEDIUM);
+		
+		
+		DateFormat mediumDateFormatEN = DateFormat.getDateTimeInstance(
+		
+		DateFormat.MEDIUM,
+		
+		DateFormat.MEDIUM, new Locale("EN","en"));
+	
+
+		
+		System.out.print(mediumDateFormat.format(aujourdhui));
+	
+		
+		System.out.print(" [NAME="+logger.getName());
 	
 		System.out.print(" | LEVEL="+level);
 		
